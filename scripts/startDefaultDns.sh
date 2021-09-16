3d58c52bfd2cbc43e0b2033ee5241305881bbbb0
yyasha <50919655+yyasha@users.noreply.github.com>
2021-09-16 19:51:04 +0300

    Update startDefaultDns.sh

--- scripts/startDefaultDns.sh
+++ scripts/startDefaultDns.sh
@@ -1,2 +1,4 @@
-iptables -t nat -I PREROUTING -i eth0 -p udp --dport 53 -j DNAT --to-destination 192.168.1.1:53
-echo 1 > /proc/sys/net/ipv4/ip_forward
+# iptables -t nat -I PREROUTING -i eth0 -p udp --dport 53 -j DNAT --to-destination 192.168.1.1:53
+iptables -t nat -I PREROUTING -i eth0 -p udp --dport 53 -j DNAT --to-destination 192.168.1.66:54
+iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 54
+# echo 1 > /proc/sys/net/ipv4/ip_forward
