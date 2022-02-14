58b50cafb11163ac4bcb17b9a83b826976f3cfb1
yyasha <50919655+yyasha@users.noreply.github.com>
2022-02-14 20:24:03 +0300

    Update startGlobalTor.sh

--- scripts/startGlobalTor.sh
+++ scripts/startGlobalTor.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-iptables -t nat -A PREROUTING -i eth0 -p tcp --syn -j REDIRECT --to-ports 9040
+# iptables -t nat -A PREROUTING -i eth0 -p tcp --syn -j REDIRECT --to-ports 9040
+iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports 9040
 echo '0' | tee /proc/sys/net/ipv4/conf/eth0/forwarding
