bc9778425daa33fd01f158fa97f4a7cb14e304cf
yyasha <50919655+yyasha@users.noreply.github.com>
2022-06-15 14:56:02 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 cd /tmp
-mkdir build && cd build
+mkdir build
+cd build
 wget https://openresty.org/download/openresty-1.19.9.1.tar.gz && tar -xzvf openresty-1.19.9.1.tar.gz
 mv openresty-1.19.9.1 openresty
+
+apt update && apt install -y iptables ipset libpcre3 libpcre3-dev zlib1g zlib1g-dev build-essential make libssl-dev ipset libpcre++-dev libcurl4 curl git
 
 git clone https://github.com/fffonion/lua-resty-openssl
 git clone https://github.com/fffonion/lua-resty-openssl-aux-module
@@ -9,21 +12,19 @@ git clone https://github.com/Evengard/lua-resty-getorigdest-module
 git clone https://github.com/iryont/lua-struct
 git clone https://github.com/Evengard/lua-resty-socks5
 
-apt update && apt install -y iptables ipset libpcre3 libpcre3-dev zlib1g zlib1g-dev build-essential make libssl-dev ipset libpcre++-dev libcurl4 curl
-
 # export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin
 
 cd openresty
 ./configure --prefix=/opt/nginxdpi --with-cc=gcc --add-module=/tmp/build/lua-resty-openssl-aux-module --add-module=/tmp/build/lua-resty-openssl-aux-module/stream --add-module=/tmp/build/lua-resty-getorigdest-module/src
 make -j4 && make install
 
-mkdir /opt/nginxdpi/
+mkdir -p /opt/nginxdpi/
 cp -r /tmp/build/lua-resty-getorigdest-module/lualib/* /opt/nginxdpi/lualib/ 
-mkdir /opt/nginxdpi/lualib/resty/
+mkdir -p /opt/nginxdpi/lualib/resty/
 cp -r /tmp/build/lua-resty-openssl/lib/resty/* /opt/nginxdpi/lualib/resty/
 cp -r /tmp/build/lua-resty-openssl-aux-module/lualib/* /opt/nginxdpi/lualib/
 cp /tmp/build/lua-resty-socks5/socks5.lua /opt/nginxdpi/lualib/resty/
 cp /tmp/build/lua-struct/src/struct.lua /opt/nginxdpi/lualib/
 
-mkdir /opt/nginxdpi/cfg/
+mkdir -p /opt/nginxdpi/cfg/
 cp ~/raspberrypi_router/DPI/nginx.conf /opt/nginxdpi/cfg/nginx.conf
